6efdc5d61a392e062a9538f872504e173d2152da
Mokshesh Dafariya <mok2709@gmail.com>
2019-09-15 18:40:40 +0530

    Code commenting

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,10 @@ const MAX_CLASS = 5;
 const webcamElement = document.getElementById('webcam-input');
 
 // Create KNN Classifier instance
+// In this we do not provide a model with weights, but rather a utility for constructing a KNN model using activations from another model or any other tensors you can associate with a class/label.
 const classifier = knnClassifier.create();
 
+// Current number of traning class available in th UI
 let numberOfActiveClasses = 2;
 
 // Color hex codes for dynamically assigning the Train button and Progress bar
@@ -26,6 +28,7 @@ async function app() {
     // Reads an image from the webcam and associates it with a specific class index.
     const addExample = classId => {
         // Get the intermediate activation of MobileNet 'conv_preds' and pass that to the KNN classifier.
+        // Add MobileNet activations to the model repeatedly for all classes.
         const activation = net.infer(webcamElement, 'conv_preds');
 
         // Pass the intermediate activation to the classifier.
@@ -43,20 +46,27 @@ async function app() {
         if (classifier.getNumClasses() > 0) {
             // Get the activation from mobilenet from the webcam.
             const activation = net.infer(webcamElement, 'conv_preds');
+
+            // Make a prediction.
             // Get the most likely class and confidences from the classifier module.
             const result = await classifier.predictClass(activation, MAX_CLASS);
             
+            // Get the confidences for the all the classes
             con = result.confidences;
+
+            // Based on the probabilities, update the width and text of the progress bar.
             $.each(result.confidences, function(index, element) {
                 $('#progress-bar-class-'+index).width((element*100) + '%');
                 $('#progress-bar-class-'+index).text((element*100) + '%');
             });
         }
 
+        // Give some breathing room by waiting for the next animation frame to fire.
         await tf.nextFrame();
     }
 }
 
+// Function to ask permission from the usesr to access the camera via browser
 async function setupWebcam() {
     return new Promise((resolve, reject) => {
         const navigatorAny = navigator;
@@ -78,6 +88,7 @@ async function setupWebcam() {
     });
 }
 
+// Show a modal window. Indicating the downloading and loading of the model
 $('#wait').modal('show');
 app();
 
@@ -90,9 +101,7 @@ $(document).ready(function () {
         $(this).css('background-color', colors[index]);
     });
 
-    // $('.class-train-section').on('click', '.btn-train', function (event) {
-    // $(this).html('Training...');
-    // });
+    // Increment the counter of the sample, whenever a samplle is captured
     $('.btn-train').on('click', function (event) {
         samples = $(this).closest('.card-body').find('.samples')[0];
         c = samples.textContent;c++;
@@ -109,6 +118,7 @@ $(document).ready(function () {
         }
     });
 
+    // Change the text in the output if the class is changed in the train section
     $('.class-title').on('change', function (event) {
         val = $(this).val();
         $('#' + $(this).data('output-id')).html(val);
